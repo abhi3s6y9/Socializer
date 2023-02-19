c876a8bb4bd3a2a6d4d99038dc4dcb3cd91f8c67
Abhi <abhi3s6y9@gmail.com>
2023-02-19 20:49:54 +0530

    setup static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Socializer</h1>');
+    // return res.end('<h1>Express is up for Socializer</h1>');
+
+    return res.render('home', {
+        title: "This is my title of home page"
+    });
 }
