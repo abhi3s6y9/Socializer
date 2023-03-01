c356dd788e7d2507ad56d0e014f3c9c1173e88ac
Abhi <abhi3s6y9@gmail.com>
2023-03-02 00:30:17 +0530

    Rectifying mistake 2

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Socializer</h1>');
 
+    // console.log(req.cookies);
+    // res.cookies('user_id', 25);
     return res.render('home', {
-        title: "This is my title of home page"
+        title: "This is the title of my home page"
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
-console.log('Users');
+// console.log('Users');
 
 router.get('/profile', usersController.profile);
+
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
+
+router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
+
+router.post('/sign-out', usersController.logout);
 
 module.exports = router;
