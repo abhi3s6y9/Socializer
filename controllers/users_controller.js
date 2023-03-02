01435e1a6ae99b94f246014d74808063ef2b83e5
Abhi <abhi3s6y9@gmail.com>
2023-03-02 12:04:59 +0530

    Add passportjs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -65,35 +65,5 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // steps to authenticate
-    // find the user
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log("Error in finding user in signing in");
-            return;
-        }
-
-        // handle user found
-
-        if(user){
-            // handle password which doesn't match
-            if(user.password != req.body.password){
-                return res.redirect('back');
-            }
-
-            // handle session creation
-            res.cookie('user_id', user.id);
-            return res.redirect('/users/profile');
-        }
-        else{
-            // handle user not found
-            return res.redirect('back');
-        }
-    });
-}
-
-// sign out the currently logged in user
-module.exports.logout = function(req, res){
-    res.clearCookie("user_id");
-    return res.redirect('/users/sign-in');
+    // TODO later
 }
