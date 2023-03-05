512d1b8d0befa8ffe4079c73b26de0271438b240
Abhi <abhi3s6y9@gmail.com>
2023-03-05 21:14:01 +0530

    Passport and some fixes

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,6 @@
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Socializer</h1>');
-
-    // console.log(req.cookies);
-    // res.cookies('user_id', 25);
+    
     return res.render('home', {
         title: "This is the title of my home page"
     });
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,27 +1,23 @@
 const User = require('../models/user');
 
-module.exports.profile = function(req, res){
+module.exports.profile = async function(req, res){
     // res.end('<h1>User Profile</h1>');
-    if(req.cookies.user_id){
-        User.findById(req.cookies.user_id, function(err, user){
-            if(user){
-                return res.render('user_profile', {
-                    title: "Users Profile",
-                    user: user
-                });
-            }
-            else{
-                return res.redirect('/users/sign-in');
-            }
-        });
-    }
-    else{
-        return res.redirect('/users/sign-in');
-    }
+    let user = await User.findById(req.user)
+        if(user){
+            return res.render('user_profile', {
+                title: "Users Profile",
+                user: user
+            });
+        }
 }
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+        res.redirect('/users/profile');
+    }
+
     return res.render('sign_in', {
         title: "Sign In"
     });
@@ -29,6 +25,11 @@ module.exports.signIn = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    if(req.isAuthenticated()){
+        res.redirect('/users/profile');
+    }
+
     return res.render('sign_up', {
         title: "Sign Up"
     });
@@ -41,12 +42,8 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log("Error in finding user in signing up");
-            return;
-        }
-
+    User.findOne({email: req.body.email})
+    .then((user) => {
         if(!user){
             User.create(req.body, function(err, user){
                 if(err){
@@ -60,10 +57,15 @@ module.exports.create = function(req, res){
         else{
             return res.redirect('back');
         }
+    })
+    .catch((err) => {
+        console.log("Error in finding user in signing up");
+        return;
     });
+
 }
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+    return res.redirect('/');
 }
