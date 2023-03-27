ef2413bf782533775ac0facd13c0bbe2656816f1
Abhi <abhi3s6y9@gmail.com>
2023-03-27 23:59:59 +0530

    Add SASS-SCSS to the Project

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,6 @@ module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Socializer</h1>');
     
     return res.render('home', {
-        title: "This is the title of my home page"
+        title: "Home page"
     });
 }
