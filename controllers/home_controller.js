d5858ab73c6c8f018f2af4171951662446d27799
Abhi <abhi3s6y9@gmail.com>
2023-04-03 18:30:10 +0530

    Show all the posts and the respective user who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,21 @@
-module.exports.home = function(req, res){
+const Post = require('../models/post');
+
+module.exports.home = async function(req, res){
     // return res.end('<h1>Express is up for Socializer</h1>');
     
+    // Populate the user of each post
+    var posts = await Post.find().populate('user').exec();
+
     return res.render('home', {
-        title: "Home page"
+        title: "Home page",
+        posts: posts
     });
+
+    // await Post.find({}).populate('user').exec(function(err, posts){
+    //     return res.render('home',{
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // });
+
 }
