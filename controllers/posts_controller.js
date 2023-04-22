abed82a6b2c582d491f581aa335cf730f8fd21fd
Abhi <abhi3s6y9@gmail.com>
2023-04-22 21:54:10 +0530

    Add flash messages to more actions i.e. Post creation and Post deletion

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -9,11 +9,13 @@ module.exports.create = async function(req, res){
             user: req.user._id
         });
     
+        req.flash('success', 'Post Published!');
         return res.redirect('back');
     }
     catch(err){
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
@@ -30,17 +32,21 @@ module.exports.destroy = async function(req, res){
             post.deleteOne(post._id);
     
             await Comment.deleteMany({post: req.params.id});
+
+            req.flash('success', 'Post and associated comments deleted!');
     
             return res.redirect('back');
         }
         else{
+            req.flash('error', "You are not authorised to delete this post");
             return res.redirect('back');
         }
 
     }
 
     catch(err){
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
